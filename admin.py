9a405c522cc6a1ba7236de15b43f96abc7e80e66
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2012-12-14 16:00:58 +0100

    Define fields parameters for admin form

--- admin.py
+++ admin.py
@@ -5,13 +5,17 @@ class ProcessAdmin(admin.ModelAdmin):
     class Meta:
         model = Process
 
+    fields = ('source', 'operation', 'parameters')
     list_display = ('id', 'owner', 'source', 'operation', 'parameters',
                     'task_id', 'created_on', 'started_on', 'finished_on',
                     'status', 'progress', 'eta')
-    list_display_links = ('id', )
     readonly_fields = ('id', 'owner',
                        'task_id', 'created_on', 'started_on', 'finished_on',
                        'status', 'progress', 'eta')
     list_filter = ('status','source', 'operation',)
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        obj.save()
 
 admin.site.register(Process, ProcessAdmin)
