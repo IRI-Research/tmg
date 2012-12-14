77c2617fa62ba047af77ac6a7620a296eb3a1e91
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2012-12-14 11:39:54 +0100

    Use operations module + implement save/delete methods

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.db.models.signals import pre_save
 from fields import JSONField
 from django.contrib.auth.models import User
-
-REGISTERED_OPERATIONS = [ ('transcode', 'Transcode'),
-                          ('shotdetect', 'Detect shots') ]
+import tmg.operations as operations
+from celery.task.control import revoke
 
 class Process(models.Model):
     """Representation of a process.
@@ -14,12 +14,14 @@ class Process(models.Model):
     CANCELLING = 'cancelling'
     FINISHED = 'finished'
     CANCELLED = 'cancelled'
+    ABORTED = 'aborted'
     STATUS_CHOICES = (
         (CREATED, 'Created'),
         (STARTED, 'Started'),
         (CANCELLING, 'Cancelling'),
         (FINISHED, 'Finished'),
         (CANCELLED, 'Cancelled'),
+        (ABORTED, 'Aborted'),
         )
 
     # Principle: a process will store its result (transcoded file,
@@ -37,7 +39,7 @@ class Process(models.Model):
     source = models.CharField(max_length=1024, help_text='Source filename for the operation')
 
     # id of the operation (transcode, shotdetect, etc)
-    operation = models.CharField(max_length=255, help_text='Operation class identifier', choices=REGISTERED_OPERATIONS)
+    operation = models.CharField(max_length=255, help_text='Operation class identifier', choices=[ (name, kl.__doc__.splitlines()[0]) for (name, kl) in operations.REGISTERED_OPERATIONS ])
     task_id = models.CharField(max_length=255, editable=False)
 
     created_on = models.DateTimeField(null=True, editable=False, auto_now=True, help_text='Process created on')
@@ -75,3 +77,36 @@ class Process(models.Model):
     def __unicode__(self):
         return u"Operation %s pk=%s on %s: %s" % (self.operation,
                                                   self.pk, self.source, self.current_status())
+
+    def save(self, *p, **kw):
+        super(Process, self).save(*p, **kw)
+        # Start the process
+        # Putting the import here works around the circular dependency
+        if self.status == self.CREATED:
+            # Start the process
+            from tasks import start_process
+            task = start_process.delay(self.pk)
+
+    def delete(self, *p, **kw):
+        """Cancelling an operation.
+
+        It will not delete the resource itself, rather put it in the
+        CANCELLING (then CANCELLED) state.
+        """
+        self.status = self.CANCELLING
+        self.save()
+        # Cancel the task
+        if self.task_id:
+            revoke(self.task_id, terminate=True)
+        else:
+            raise Exception("No associated task")
+
+    def start(self):
+        """Start the process.
+        """
+        op = operations.REGISTERED_OPERATIONS[self.operation](source=self.source,
+                                                              destination=None,
+                                                              parameters=self.parameters,
+                                                              progress_callback=None,
+                                                              finish_callback=None)
+        op.start()
