de5ae7ab9e1231446f24702af8b614add986041e
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2012-12-14 15:23:36 +0100

    Make source, operation and parameters editable

--- admin.py
+++ admin.py
@@ -9,10 +9,9 @@ class ProcessAdmin(admin.ModelAdmin):
                     'task_id', 'created_on', 'started_on', 'finished_on',
                     'status', 'progress', 'eta')
     list_display_links = ('id', )
-    readonly_fields = ('id', 'source', 'operation', 'parameters',
+    readonly_fields = ('id', 'owner',
                        'task_id', 'created_on', 'started_on', 'finished_on',
                        'status', 'progress', 'eta')
     list_filter = ('status','source', 'operation',)
 
 admin.site.register(Process, ProcessAdmin)
-
