eb036fafc1411ed78abbe8701ff6dcb9f74d097f
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2012-12-14 15:14:37 +0100

    Add Admin registration

---
+++ admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin, messages
+from models import Process
+
+class ProcessAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Process
+
+    list_display = ('id', 'owner', 'source', 'operation', 'parameters',
+                    'task_id', 'created_on', 'started_on', 'finished_on',
+                    'status', 'progress', 'eta')
+    list_display_links = ('id', )
+    readonly_fields = ('id', 'source', 'operation', 'parameters',
+                       'task_id', 'created_on', 'started_on', 'finished_on',
+                       'status', 'progress', 'eta')
+    list_filter = ('status','source', 'operation',)
+
+admin.site.register(Process, ProcessAdmin)
+
