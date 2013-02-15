74f854b7e5c61b4138bf1ec06b16281804efbdf5
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2013-02-15 15:20:10 +0100

    Cleanup imports

--- tmg/views.py
+++ tmg/views.py
@@ -4,12 +4,9 @@ from tmg.permissions import IsOwnerOrReadOnly
 
 from rest_framework import generics
 from rest_framework import permissions
-
-from rest_framework import renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from rest_framework import renderers
 
 @api_view(('GET',))
 def api_root(request, format=None):
