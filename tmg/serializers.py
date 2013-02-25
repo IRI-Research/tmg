4b7fa41a133875401140646497f200ffd3a80112
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2013-02-25 10:26:16 +0100

    Remove eta and add output in serialization

--- tmg/serializers.py
+++ tmg/serializers.py
@@ -10,14 +10,14 @@ class ProcessSerializer(serializers.HyperlinkedModelSerializer):
     parameters = serializers.CharField(required=False)
 
     id = serializers.CharField(source='pk', read_only=True)
+    output = serializers.CharField(read_only=True)
     task_id = serializers.CharField(read_only=True)
     created_on = serializers.DateTimeField(read_only=True)
     started_on = serializers.DateTimeField(read_only=True)
     finished_on = serializers.DateTimeField(read_only=True)
     status = serializers.IntegerField(read_only=True)
     progress = serializers.IntegerField(read_only=True)
-    eta = serializers.CharField(read_only=True)
 
     class Meta:
         model = models.Process
-        fields = ('id', 'url', 'owner', 'source', 'operation', 'parameters', 'task_id', 'created_on', 'started_on', 'finished_on', 'status', 'progress', 'eta')
+        fields = ('id', 'url', 'owner', 'source', 'operation', 'parameters', 'task_id', 'created_on', 'started_on', 'finished_on', 'status', 'progress', 'output')
