aef2f977a604dfd94239b6f1c436f0f9dfbc49d4
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2013-02-25 11:08:54 +0100

    Add license header to source files

--- tmg/states.py
+++ tmg/states.py
@@ -1,2 +1,14 @@
+#
+# Copyright Olivier Aubert <contact@olivieraubert.net> 2013
+#
+# This software offers a media transcoding and processing platform.
+#
+# TMG is a free software subjected to a double license.
+# You can redistribute it and/or modify if you respect the terms of either
+# (at least one of both licenses):
+# - the GNU Lesser General Public License as published by the Free Software Foundation;
+#   either version 3 of the License, or any later version.
+# - the CeCILL-C as published by CeCILL; either version 2 of the License, or any later version.
+#
 PROGRESS = 'TMG_PROGRESS'
 FINISH = 'TMG_FINISH'
